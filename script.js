2541aac8de7ad04311e6866b1446481d77277b57
cornerstonetacticalaz <cornerstonetacticalaz@gmail.com>
2026-07-05 21:53:28 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const nav = document.querySelector("nav");
 menu.addEventListener("click", () => {
     nav.classList.toggle("show");
 });
+
+document.querySelectorAll(".faq-question").forEach(button => {
+    button.addEventListener("click", () => {
+        const answer = button.nextElementSibling;
+
+        answer.classList.toggle("open");
+    });
+});
